c59aeba0a82228f753c86abb1307b36302cb11ab
chris <chrissalvado@hotmail.com>
2019-06-11 13:28:20 -0500

    build

---
+++ build/precache-manifest.863ebccbfbee348a4cc77fd0a5b4450e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003c8123cc40dcb3fc855e1110aa7ffb",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "ec4f5a03b3e3e16ebe32",
+    "url": "/Clicky/static/css/main.13c0a76c.chunk.css"
+  },
+  {
+    "revision": "edf26db9c6fc8ca6354e",
+    "url": "/Clicky/static/js/2.253d07d0.chunk.js"
+  },
+  {
+    "revision": "ec4f5a03b3e3e16ebe32",
+    "url": "/Clicky/static/js/main.89b91ecb.chunk.js"
+  },
+  {
+    "revision": "1726ed0b9997b87a72d7",
+    "url": "/Clicky/static/js/runtime~main.a36ce3a7.js"
+  },
+  {
+    "revision": "8199f1869d8dea64fd6d768625def35e",
+    "url": "/Clicky/static/media/pj1.8199f186.jpg"
+  },
+  {
+    "revision": "5a708af8874e72c9c82360f83e0627f7",
+    "url": "/Clicky/static/media/pj10.5a708af8.jpg"
+  },
+  {
+    "revision": "517dd488ad36c43e4f596534d4bd4276",
+    "url": "/Clicky/static/media/pj11.517dd488.jpg"
+  },
+  {
+    "revision": "b7e6614a742a3a88864f5e6943bd30c2",
+    "url": "/Clicky/static/media/pj12.b7e6614a.jpg"
+  },
+  {
+    "revision": "802da5c8a3f802bf798943f531aa3651",
+    "url": "/Clicky/static/media/pj13.802da5c8.jpg"
+  },
+  {
+    "revision": "270c3dae9039a709e1bbc6bc14e228c7",
+    "url": "/Clicky/static/media/pj14.270c3dae.jpg"
+  },
+  {
+    "revision": "d635b60642b83d5f3d5bc1c1422f7a3e",
+    "url": "/Clicky/static/media/pj2.d635b606.png"
+  },
+  {
+    "revision": "665306a126cfb12f93d253d0ea27ae15",
+    "url": "/Clicky/static/media/pj3.665306a1.png"
+  },
+  {
+    "revision": "e3be1658fbd370d9d2e0f10e1bdd23e0",
+    "url": "/Clicky/static/media/pj4.e3be1658.jpg"
+  },
+  {
+    "revision": "13341404e6590b8e8f64b641231499a3",
+    "url": "/Clicky/static/media/pj5.13341404.jpg"
+  },
+  {
+    "revision": "08d0030f902d25479d4a94a1c1d3fbf6",
+    "url": "/Clicky/static/media/pj6.08d0030f.jpg"
+  },
+  {
+    "revision": "1c685e7c8dd85ed27ce3dabb26e6d62c",
+    "url": "/Clicky/static/media/pj7.1c685e7c.jpg"
+  },
+  {
+    "revision": "3a6d809b18ca0d9ec70c7eb745cf517e",
+    "url": "/Clicky/static/media/pj8.3a6d809b.png"
+  },
+  {
+    "revision": "6cc3d30be19c13aab8df92784c96e9a2",
+    "url": "/Clicky/static/media/pj9.6cc3d30b.png"
+  }
+]);
